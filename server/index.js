0475e6a8b8b776c7c8fa6aab82cd3742d2272e65
Brian Gouws <b.gouws@quidco.com>
2016-08-08 16:22:26 +0100

    Added env vars and mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Constants
-const PORT = 3000;
+const PORT = process.env.PORT;
+const DB_CONNECTION_NAME = process.env.DB_CONNECTION_NAME;
+const DB_COLLECTION_NAME = process.env.DB_COLLECTION_NAME;
+
+mongoose.connect('mongodb://' + process.env.DB_CONNECTION_NAME + '/' + DB_COLLECTION_NAME);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // App
 const app = express();
