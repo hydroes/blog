aeaae6adefdd0c265bb68c206d875684ff831e51
Brian Gouws <briangouws@hotmail.com>
2019-02-05 15:03:37 +0000

    style: re-adding pure as a package

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,31 @@
 import Link from "next/link";
 
 const Header = () => (
-  <>
-    <style>{`
-      div {
-        text-align: center;
-      }
-    `}</style>
+  <div>
     <div>
-      <h1>
-        Life of <span>Brian.</span>
-      </h1>
-      <p>
-        Hi there, I am a <span>fullstack</span> web developer
-      </p>
-      <Link href="/about">
-        <div className="find-out-more">
-          <a>
-            <span>Find out more</span>
-          </a>
-        </div>
+      <Link href="/">
+        <h1>
+          Life of <span>Brian.</span>
+        </h1>
       </Link>
+      <p>
+        A <span>fullstack</span> web developer based in London UK.
+      </p>
 
       <style jsx>{`
+        div {
+          text-align: center;
+        }
         h1 {
           margin: auto;
+          width: 60%;
           color: white;
           border: 5px;
           font-size: 6em;
+          font-weight: 500;
+        }
+        h1:hover {
+          cursor: pointer;
         }
         h1 span {
           color: rgba(255, 205, 60, 1);
@@ -38,49 +36,12 @@ const Header = () => (
           text-align: center;
           color: rgba(255, 255, 255, 1);
         }
-
         p span {
           color: rgba(255, 205, 60, 1);
         }
-
-        .find-out-more {
-          width: 160px;
-          height: 50px;
-          margin: auto;
-          background: rgba(255, 205, 60, 1);
-          border: 2px solid rgba(255, 205, 60, 1);
-          transition: 0.3s ease;
-        }
-
-        .find-out-more:hover {
-          background: transparent;
-        }
-
-        .find-out-more:hover > a > span {
-          color: white;
-        }
-
-        .find-out-more > a {
-          display: block;
-          cursor: pointer;
-          width: 100%;
-          // height: 100%;
-          text-decoration: none;
-        }
-        .find-out-more > a > span {
-          padding: 17px 0px;
-          font-weight: 900;
-          font-size: 14px;
-          text-align: center;
-          letter-spacing: px;
-          text-transform: uppercase;
-          color: rgba(1, 1, 1, 1);
-          transition: 0.3s ease;
-          display: block;
-        }
       `}</style>
     </div>
-  </>
+  </div>
 );
 
 export default Header;
